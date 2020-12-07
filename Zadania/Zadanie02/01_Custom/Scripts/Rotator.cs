4c3935320bb2c45bb84788fdf84c1ffd28139bae
xkirschova <xkirschova@stuba.sk>
2020-12-07 19:19:51 +0100

    e

--- Zadania/Zadanie02/01_Custom/Scripts/Rotator.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-// otacanie coinov
-public class Rotator : MonoBehaviour {
-
-    public float speed;
-    void Update () { transform.Rotate(Vector3.up * speed * Time.deltaTime); }
-}
