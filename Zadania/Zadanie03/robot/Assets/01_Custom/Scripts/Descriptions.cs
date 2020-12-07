512b0686ba3e59a568da500a8c75ba987acede4f
xkirschova <xkirschova@stuba.sk>
2020-12-07 18:51:31 +0100

    z3 in progress

---
+++ Zadania/Zadanie03/robot/Assets/01_Custom/Scripts/Descriptions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Descriptions : MonoBehaviour {
+
+    public GameObject description;
+    public void ToggleDescription() {
+        
+        if (description.activeSelf) {
+            description.SetActive(false);
+        } else {
+            description.SetActive(true);
+            
+        }
+    }
+
+    
+}
