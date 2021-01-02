f4b25a66a8ee108b2930c006ab9185fcc220f78a
xkirschova <xkirschova@stuba.sk>
2021-01-02 13:54:46 +0100

    sk

---
+++ Zadania/Skuska/New Unity Project/Assets/01 Custom/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class GameManager : MonoBehaviour {
+    public Camera cam;
+    public Texture2D cursorSelect;
+    public Texture2D cursorPointer;
+    // Start is called before the first frame update
+    void Start() {
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(ray,out hit, 100.0f)) {
+           
+            if (hit.transform.CompareTag("Interactable")) {
+                Cursor.SetCursor(cursorSelect, Vector2.zero,CursorMode.Auto);
+            }
+            else {
+                Cursor.SetCursor(cursorPointer, Vector2.zero,CursorMode.Auto);
+            }
+        }
+    }
+}
